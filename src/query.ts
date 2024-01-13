67c8c64c2f95318cfeb94aa79b17fcdc1f22cc0c
Yizhao He <heyizhaobupt@gmail.com>
2024-01-13 14:44:36 -0800

    Return wiki summary for query

--- src/query.ts
+++ src/query.ts
@@ -1,11 +1,16 @@
 import wiki from 'wikipedia';
 
-async function fetchWiki(query: string) {
+async function fetchWiki(query: string): Promise<string> {
   console.log('Searching for', query);
-  const searchResults = await wiki.search('Batma');
+  const searchResults = await wiki.search(query);
   for (const result of searchResults.results) {
     console.log(result);
+    const page = await wiki.page(result.title);
+    const summary = await page.summary();
+    console.log(summary.extract);
+    return summary.extract;
   }
+  return '';
 }
 
 async function main() {
