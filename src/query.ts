ec63cb9e836fbe9b60930feaf5c5b732b51d5395
Yizhao He <heyizhaobupt@gmail.com>
2024-01-13 16:04:14 -0800

    Use whole page content from wikipedia

--- src/query.ts
+++ src/query.ts
@@ -6,9 +6,15 @@ export async function fetchWiki(query: string) {
   for (const result of searchResults.results) {
     console.log(result);
     const page = await wiki.page(result.title);
-    const summary = await page.summary();
-    console.log(summary.extract);
-    return summary.extract;
+    /* Return short page summary */
+    // const summary = await page.summary();
+    // console.log(summary.extract);
+    // return summary.extract;
+
+    /* Return all page content. */
+    const content = await page.content();
+    console.log(content);
+    return content;
   }
   return '';
 }
